d51d6b98dbf2b865f4a40f003afc69602d53500c
Yassin Mziya <ymm22@cornell.edu>
2018-04-12 22:30:02 -0400

    basically done

--- src/Test.js
+++ src/Test.js
@@ -2,9 +2,19 @@ import React from 'react';
 
 import PageWrap from './components/PageWrap';
 import Rankings from './components/Rankings';
+import Radar from './components/RadarWrap';
 
 export default class Test extends React.Component {
     render() {
-        return <Rankings />
+        return (
+            <Radar 
+                countries={["TZA","CHE","USA"]} 
+                year={2017} 
+                indicators={["1.", "2.", "3.", "4.", "5."]}
+                height={600}
+                width={600}
+                padding={100}
+            />
+        )
     }
 }
